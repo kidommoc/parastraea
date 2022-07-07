f12c1e0de1fa21d1cc608ee833184cba59fc9138
km <kidommoc@gmail.com>
2022-07-07 17:39:16 +0800

    add pub/dev environment distinguishing flag

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,10 @@ if (envfound.error) {
     throw new Error("no .env file!")
 }
 
+process.env.ENV = process.env.ENV || 'development'
+
 export default {
+    environment: process.env.ENV,
     port: parseInt(process.env.PORT, 10),
     mongodbUri: process.env.MONGODB_URI,
 }
