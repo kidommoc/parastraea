6c40c88a306436fa65edd6c2527d775a14d598e9
km <kidommoc@gmail.com>
2022-07-12 07:47:17 +0800

    add project root path in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import path from 'path'
 
 const envfound = dotenv.config()
 if (envfound.error) {
@@ -6,9 +7,11 @@ if (envfound.error) {
 }
 
 process.env.ENV = process.env.ENV || 'development'
+process.env.ROOT_PATH = path.join(path.dirname(require.main.filename), '..')
 
 export default {
     environment: process.env.ENV,
     port: parseInt(process.env.PORT, 10),
+    rootPath: process.env.ROOT_PATH,
     mongodbUri: process.env.MONGODB_URI,
 }
